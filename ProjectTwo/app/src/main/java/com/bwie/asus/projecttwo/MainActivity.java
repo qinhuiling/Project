0080f6d024e9236e5bf37ffa059c91ff07bf9f1d
qinhuiling <2033149744@qq.com>
2017-11-07 13:48:51 +0800

    第二次上传

--- ProjectTwo/app/src/main/java/com/bwie/asus/projecttwo/MainActivity.java
+++ ProjectTwo/app/src/main/java/com/bwie/asus/projecttwo/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //调用方法
         initView();
         initData();
 
