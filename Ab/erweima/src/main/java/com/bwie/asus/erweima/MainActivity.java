1cc4d876e6852a7bbd59f70843f4f2172acf6a67
qinhuiling <2033149744@qq.com>
2017-11-08 20:06:02 +0800

    二维码扫描

---
+++ Ab/erweima/src/main/java/com/bwie/asus/erweima/MainActivity.java
@@ -0,0 +1,36 @@
+package com.bwie.asus.erweima;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivityForResult(intent,100);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == 200){
+            String text = data.getStringExtra("text");
+            Toast.makeText(MainActivity.this,"扫描成功",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
